89196230db2ded36c0b8ded2501f772540c86068
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-06 14:10:30 -0800

    update cluster.py to include the correct residues

--- hw2skeleton/cluster.py
+++ hw2skeleton/cluster.py
@@ -1,5 +1,44 @@
 from .utils import Atom, Residue, ActiveSite
 
+# I consider each residue in my active sites to embody 4 different physiochemical properties--
+#this means each of the 20 amino acids is now condensed to an alphabet of just 6 letters.
+#these properties are: L = large, A= acidic, B= basic, N= neutral, F= aliphatic, P= polar
+#By reducing the features of my active sites from 20 to 6, I hope to find trends in composition across active sites.
+#Similar composition and similar size would suggest similar ligands (the things that bind to active sites), and thus 
+#similar biological function.
+
+#Here's a dictionary of those residues with their corresponding physiochemical definitions:
+convert = {'ARG': 'B', 'LYS': 'B', 'ASP': 'A', 'GLN': 'P', 'ASN': 'P', 'GLU': 'A',
+'HIS': 'B', 'SER': 'P', 'THR': 'P', 'PRO': 'N', 'TYR': 'P', 'CYS': 'P', 'GLY': 'N', 'ALA': 'F',
+'MET': 'N', 'TRP': 'L', 'LEU': 'F', 'VAL': 'A', 'PHE': 'L', 'ILE': 'F'}
+
+#Now I need to extract all of those active sites from the data folder, using those utility functions.
+#First call the function that reads all active sites in a folder.
+
+#So first I extract the residues from each active site, make them a list, then add them to some temporary list of all active sites.
+#Next I need to change each of those residue names into my own kind.
+#The residues are still separated, and I need them in one string, so join them together.
+#Finally, I get them into the format I used to start making my similarity matrix. 
+My_active_Site = read_active_sites("../data/")
+temp = []
+for site in My_active_Site:
+    temp_physio = []
+    amino_acid_list = site.residues
+    for amino_acid in amino_acid_list:
+        temp_physio.append(amino_acid.type)
+    temp.append(temp_physio)
+new_temp = []
+for item in temp:
+    new_temp_physio = []
+    for aa in item:
+        new_temp_physio.append(convert[aa])
+    new_temp.append(new_temp_physio)
+new_super_temp = []
+for almost in new_temp:
+    new_super_temp.append(''.join(almost))
+my_lame_list = [ [x] for x in new_super_temp]
+print(my_lame_list)
+
 def compute_similarity(site_a, site_b):
     """
     Compute the similarity between two given ActiveSite instances.
