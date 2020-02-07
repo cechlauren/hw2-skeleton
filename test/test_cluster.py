ee8ddaef9e0007d769cda631304b28437f8df936
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-06 20:04:14 -0800

    update test_cluster.py for test distances stmt

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -12,6 +12,24 @@ def test_similarity():
     # update this assertion
     assert cluster.compute_similarity(activesite_a, activesite_b) == 0.0
 
+#def test_distance():
+   # filename_a = os.path.join("data", "276.pdb")
+   # filename_b = os.path.join("data", "4629.pdb")
+
+   # activesite_a = io.read_active_site(filename_a)
+   # activesite_b = io.read_active_site(filename_b)
+
+    # update this assertion
+   # simulation1 = test distance(activesite_a, activesite_a)
+   # simulation2 = test distance(activesite_a, activesite_a)
+   # simulation3 = test distance(activesite_a, activesite_b)
+   # simulation4 = test distance(activesite_b, activesite_a)
+    
+   # assert simulation1 == simulation2
+   # assert simulation3 == simulation4
+    
+    
+    
 def test_partition_clustering():
     # tractable subset
     pdb_ids = [276, 4629, 10701]
